13df1f49601395eff02eccf96c24b083b093941d
SystemSolution21 <156997764+SystemSolution21@users.noreply.github.com>
2024-11-05 22:40:14 +0900

    modified

--- Libraries/lib_pipe.py
+++ Libraries/lib_pipe.py
@@ -1,15 +1,25 @@
 from pipe import select, where
 
+
 def list_square_even_numbers(num_range: int) -> list[int]:
     num_list = range(num_range)
-    result: list[int] = list(num_list | where(lambda x: x % 2 == 0) | select(lambda x: x * 2))
+    result: list[int] = list(
+        num_list | where(lambda x: x % 2 == 0) | select(lambda x: x**2)
+    )
     return result
 
+
 def main() -> None:
-    while (text := input("Input number to display list of square of even numbers or 'q' to quit: ")) != 'q':
+    while (
+        text := input(
+            "Input number to display list of square of even numbers or 'q' to quit: "
+        )
+    ) != "q":
         if text.isdigit():
             square_even_numbers: list[int] = list_square_even_numbers(int(text))
-            print(f'List of square of even numbers: {square_even_numbers}')
+            print(
+                f"List of square of even numbers of input {text} is: {square_even_numbers}"
+            )
         else:
             print("Invalid Input!")
 
