6379d446d541857298960251b098f7e49e26ecbb
SystemSolution21 <156997764+SystemSolution21@users.noreply.github.com>
2025-03-15 00:30:31 +0900

    new file:   Tips/series.ipynb         new file:   gil_jit/README.md         new file:   gil_jit/gil.py

---
+++ gil_jit/README.md
@@ -0,0 +1,19 @@
+# GIL
+
+python 3.13.2
+gil is active
+single threaded compute done.
+Execution time of single_threaded_compute: 8.155 seconds
+multi threaded compute done.
+Execution time of multi_threaded_compute: 8.200 seconds
+multi processing compute done.
+Execution time of multi_processing_compute: 3.632 seconds
+
+python 3.13.2+freethreaded
+gil is disabled
+single threaded compute done.
+Execution time of single_threaded_compute: 8.282 seconds
+multi threaded compute done.
+Execution time of multi_threaded_compute: 4.110 seconds
+multi processing compute done.
+Execution time of multi_processing_compute: 3.668 seconds
---
+++ gil_jit/gil.py
@@ -0,0 +1,102 @@
+import sys
+import sysconfig
+import math
+import time
+import threading
+import multiprocessing
+
+
+# Execution time decorator
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start_time: float = time.time()
+        result = func(*args, **kwargs)
+        end_time: float = time.time()
+        execution_time: float = end_time - start_time
+        print(f"Execution time of {func.__name__}: {execution_time:.3f} seconds")
+        return result
+
+    return wrapper
+
+
+# Compute Factorial
+def compute_factorial(n):
+    return math.factorial(n)
+
+
+# Single Threaded
+@timeit
+def single_threaded_compute(num_list: list[int]):
+    for num in num_list:
+        compute_factorial(num)
+
+    print("single threaded compute done.")
+
+
+# Multi Threaded
+@timeit
+def multi_threaded_compute(num_list: list[int]):
+    threads: list[threading.Thread] = []
+
+    # Create threads
+    for num in num_list:
+        thread = threading.Thread(target=compute_factorial, args=(num,))
+        threads.append(thread)
+        thread.start()
+
+    # Wait for the threads to finish
+    for thread in threads:
+        thread.join()
+
+    print(f"multi threaded compute done.")
+
+
+# Multi Processing
+@timeit
+def multi_processing_compute(num_list: list[int]):
+    processes: list[multiprocessing.Process] = []
+
+    # Create processes
+    for num in num_list:
+        process = multiprocessing.Process(target=compute_factorial, args=(num,))
+        processes.append(process)
+        process.start()
+
+    # Wait for the processes to finish
+    for process in processes:
+        process.join()
+
+    print("multi processing compute done.")
+
+
+# Main
+def main() -> None:
+    # Python Version
+    print(f"python version: {sys.version}")
+
+    # GIL Status
+    gil_status = sysconfig.get_config_var(name="Py_GIL_DISABLED")
+
+    if gil_status is None:
+        print("gil cannot be disabled")
+    elif gil_status == 0:
+        print("gil is active")
+    elif gil_status == 1:
+        print("gil is disabled")
+
+    # List of Numbers
+    num_list: list[int] = [100_000, 200_000, 300_000, 400_000, 500_000]
+
+    # Execute Single Thread
+    single_threaded_compute(num_list=num_list)
+
+    # Execute Multi Thread
+    multi_threaded_compute(num_list=num_list)
+
+    # Execute Multi Process
+    multi_processing_compute(num_list=num_list)
+
+
+if __name__ == "__main__":
+
+    main()
