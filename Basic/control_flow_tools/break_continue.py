bd0d402510b3ff278dbf39dc872408293a995eb7
SystemSolution21 <systemsolution21@gmail.com>
2026-05-10 16:16:36 +0900

    modified: Basic/control_flow_tools/break_continue.py

--- Basic/control_flow_tools/break_continue.py
+++ Basic/control_flow_tools/break_continue.py
@@ -1,13 +1,25 @@
-# break and continue statements
+print("# break and continue statements")
 
-for n in range(2, 10):
+for n in range(2, 100):
     for x in range(2, n):
         if n % x == 0:
-            print(n, "equals", x, "*", n // x)
+            if n % 2 == 0:
+                print(x, "*", n // x, "equals", n, "and is an even number")
+                break
+            else:
+                print(x, "*", n // x, "equals", n, "and is an odd number")
             break
+    else:
+        # This 'else' belongs to the FOR loop, not the IF.
+        # It runs only if the loop finishes WITHOUT hitting 'break'.
+        if n % 2 == 0:
+            print(n, "is a prime number and even")
+        else:
+            print(n, "is a prime number and odd")
 
 
-# Even and Odd numbers
+print("\n# Even and Odd numbers")
+
 for n in range(2, 10):
     if n % 2 == 0:  # if even number
         print(f"found an even number {n}")
